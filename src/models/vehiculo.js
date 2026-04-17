0712e110c6eb5ee1e41d786051c36926f972bd85
Ignacio <ignacionazar9@gmail.com>
2026-04-17 13:12:33 -0300

    agregue el modelo del vehiculo

---
+++ src/models/vehiculo.js
@@ -0,0 +1,59 @@
+import mongoose, { Schema } from "mongoose";
+
+const vehiculoSchema = new Schema({
+  marca: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 100,
+  },
+  modelo: {
+    required: true,
+    minlength: 2,
+    maxlength: 100,
+    match: [/^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+$/],
+  },
+  anio: {
+    type: Number,
+    required: true,
+    min: 1900,
+    max: new Date().getFullYear(),
+  },
+  categoria: {
+    type: String,
+    required: true,
+    enum: ["Sedán", "SUV", "Camioneta", "Deportivo"],
+  },
+  precio: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  km: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  disponible: {
+    type: Boolean,
+    default: true,
+  },
+  descripcion: {
+    type: String,
+    minlength: 10,
+    maxlength: 500,
+  },
+  imagenes: {
+    type: [String],
+    required: true,
+    validate: {
+      validator: function (v) {
+        return v.length > 0;
+      },
+    },
+  },
+});
+
+const Vehiculo = mongoose.model("vehiculo", vehiculoSchema);
+
+export default Vehiculo;
