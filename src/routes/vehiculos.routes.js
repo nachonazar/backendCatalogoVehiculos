1252413842bbee121598e7313f0447771c781bc5
Ignacio <ignacionazar9@gmail.com>
2026-05-06 13:29:15 -0300

    agregue verificacion de JWT y proteccion de rutas de vehiculos

--- src/routes/vehiculos.routes.js
+++ src/routes/vehiculos.routes.js
@@ -1,11 +1,26 @@
 import { Router } from "express";
-import { prueba, leerVehiculos, crearVehiculo, leerVehiculosPorId, editarVehiculosPorId, borrarVehiculosPorId } from "../controllers/vehiculos.controllers.js";
+import {
+  prueba,
+  leerVehiculos,
+  crearVehiculo,
+  leerVehiculosPorId,
+  editarVehiculosPorId,
+  borrarVehiculosPorId,
+} from "../controllers/vehiculos.controllers.js";
 import validacionVehiculo from "../middlewares/validarVehiculo.js";
+import verificarJWT from "../middlewares/verificarJWT.js";
 
 const router = Router();
 //get, post, put, delete
 router.route("/prueba").get(prueba);
-router.route("/").get(leerVehiculos).post(validacionVehiculo, crearVehiculo);
-router.route("/:id").get(leerVehiculosPorId).put(validacionVehiculo, editarVehiculosPorId).delete(borrarVehiculosPorId)
+router
+  .route("/")
+  .get(leerVehiculos)
+  .post([verificarJWT, validacionVehiculo], crearVehiculo);
+router
+  .route("/:id")
+  .get(leerVehiculosPorId)
+  .put([verificarJWT, validacionVehiculo], editarVehiculosPorId)
+  .delete(verificarJWT, borrarVehiculosPorId);
 
 export default router;
